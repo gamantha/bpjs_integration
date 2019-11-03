99ba8f0bf4109ce0a09399240e337263beda7ad4
Samsul Arifin <arifinsam.sam@gmail.com>
2019-11-03 23:57:20 +0700

    migration file for diagnosis table

---
+++ database/migrations/2019_11_03_145354_create_diagnosis_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiagnosisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diagnosis', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('cat_code');
+            $table->string('code');
+            $table->string('name');
+            $table->string('cat_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('diagnosis');
+    }
+}
